4900eb2c2f08f3c12cb5024f19adb0c25b0c6e52
Nsmith4789 <156531219+Nsmith4789@users.noreply.github.com>
2024-05-03 00:23:02 -0500

    updated answer 3 to 7

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 // Question 3: Why does the counter start at NaN?
-let count = parseInt(document.getElementById('counter-value').innerText);
-
+let count = parseInt(document.getElementById('counter-value').innerText = 0);
+// has no value
 const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
 const resetButton = document.getElementById('reset');
 
 // Question 4: What happens to the counter value in the HTML when increment is clicked?
-incrementButton.addEventListener('click', function() {
-  debugger
+incrementButton.addEventListener('click', function () {
     count++;
-    document.getElementById('counter-value').innerText = count;
+    document.getElementById('counterer-value').innerText = count;
 });
+//the debugger was pausing the 
 
 // Question 5: Why does the decrement button not work?
-decrementButton.addEventListener('click', function() {
+decrementButton.addEventListener('click', function () {
     count--;
-    document.getElementById('count-value').innerText = count;
-});
+    document.getElementById('counter-value').innerText = count;
+}); //id wrong spelling
 
 // Question 6: Why does the reset function set the count to '0' as a string?
-resetButton.addEventListener('click', function() {
-    count = '0';
+resetButton.addEventListener('click', function () {
+    count = 0;
     document.getElementById('counter-value').innerText = count;
-});
+}); //removed the string from count
 
 // Question 7: How can you monitor the value of count using breakpoints?
+// set break points on event listener
+
 // Question 8: What is the scope of the count variable in DevTools?
+
 
 // Question 9: Use the Network tab to find out: Is the app.js file loaded successfully?
 // Question 10: Use the Console to track errors. Are there any errors being thrown?
